d108731b8699ed776421f5b1c5339c601dd41bf4
Michael Denomy <mdenomy@gmail.com>
2015-08-15 13:33:55 -0400

    Simple alpha [A-Za-z] encryption/decryption
    * Basic encrypt/decrypt
    * Assume only alpha characters (ignore digits, whitespace, punctuation)
    * Test large (non-overflow) shifts
    * Test negative shifts

--- lib/caesar_cypher.rb
+++ lib/caesar_cypher.rb
@@ -1,2 +1,39 @@
 class CaesarCypher
+  LOWER_A_ORD = "a".ord
+  UPPER_A_ORD = "A".ord
+  PATTERN = /[a-zA-Z]/
+
+  def initialize(shift)
+    @shift = shift
+  end
+
+  def encrypt(input)
+    input.gsub(PATTERN) {|s| encrypt_char(s) }
+  end
+
+  def decrypt(input)
+    input.gsub(PATTERN) {|s| decrypt_char(s) }
+  end
+
+  private
+
+  def base_ordinal_val(char)
+    if char.ord >= LOWER_A_ORD
+      LOWER_A_ORD
+    else
+      UPPER_A_ORD
+    end
+  end
+
+  def encrypt_char(char)
+    base = base_ordinal_val(char)
+    offset = (char.ord - base + @shift) % 26
+    (base + offset).chr
+  end
+
+  def decrypt_char(char)
+    base = base_ordinal_val(char)
+    offset = (char.ord - base - @shift) % 26
+    (base + offset).chr
+  end
 end
--- spec/caesar_cypher_spec.rb
+++ spec/caesar_cypher_spec.rb
@@ -1,4 +1,62 @@
 require 'spec_helper'
 
 describe CaesarCypher do
+
+  describe "encryption" do
+    before do
+     @cypher = CaesarCypher.new(3)
+    end
+
+    it "shifts alpha characters" do
+      expect(@cypher.encrypt("abcd")).to eq "defg"
+      expect(@cypher.decrypt("defg")).to eq "abcd"
+    end
+
+    it "wraps after z" do
+      expect(@cypher.encrypt("xyz")).to eq "abc"
+    end
+
+    it "handles upper and lower characters" do
+      expect(@cypher.encrypt("aBcXyZ")).to eq "dEfAbC"
+    end
+
+    it "leaves non alpha characters alone" do
+      expect(@cypher.encrypt("123 ,{|}")).to eq "123 ,{|}"
+    end
+  end
+
+  describe "decryption" do
+    before do
+     @cypher = CaesarCypher.new(3)
+    end
+
+    it "shifts alpha characters" do
+      expect(@cypher.decrypt("defg")).to eq "abcd"
+    end
+
+    it "wraps after z" do
+      expect(@cypher.decrypt("abc")).to eq "xyz"
+    end
+
+    it "handles upper and lower characters" do
+      expect(@cypher.decrypt("dEfAbC")).to eq "aBcXyZ"
+    end
+
+    it "leaves non alpha characters alone" do
+      expect(@cypher.decrypt("123 ,{|}")).to eq "123 ,{|}"
+    end
+  end
+
+  it "works with larger shifts" do
+    cypher = CaesarCypher.new(54)
+    expect(cypher.encrypt("abcd")).to eq "cdef"
+    expect(cypher.decrypt("cdef")).to eq "abcd"
+  end
+
+  it "works with negative shifts" do
+    cypher = CaesarCypher.new(-2)
+    expect(cypher.encrypt("abcd")).to eq "yzab"
+    expect(cypher.decrypt("yzab")).to eq "abcd"
+  end
+
 end
