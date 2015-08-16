ac1bf2663a4215d62b1eef9e42434ee7d98d4c63
Michael Denomy <mdenomy@gmail.com>
2015-08-16 10:31:08 -0400

    Cleaned up all misspelled ciphers

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -4,59 +4,59 @@ describe CaesarCipher do
 
   describe "encryption" do
     before do
-     @cypher = CaesarCipher.new(3)
+     @cipher = CaesarCipher.new(3)
     end
 
     it "shifts alpha characters" do
-      expect(@cypher.encrypt("abcd")).to eq "defg"
-      expect(@cypher.decrypt("defg")).to eq "abcd"
+      expect(@cipher.encrypt("abcd")).to eq "defg"
+      expect(@cipher.decrypt("defg")).to eq "abcd"
     end
 
     it "wraps after z" do
-      expect(@cypher.encrypt("xyz")).to eq "abc"
+      expect(@cipher.encrypt("xyz")).to eq "abc"
     end
 
     it "handles upper and lower characters" do
-      expect(@cypher.encrypt("aBcXyZ")).to eq "dEfAbC"
+      expect(@cipher.encrypt("aBcXyZ")).to eq "dEfAbC"
     end
 
     it "leaves non alpha characters alone" do
-      expect(@cypher.encrypt("123 ,{|}")).to eq "123 ,{|}"
+      expect(@cipher.encrypt("123 ,{|}")).to eq "123 ,{|}"
     end
   end
 
   describe "decryption" do
     before do
-     @cypher = CaesarCipher.new(3)
+     @cipher = CaesarCipher.new(3)
     end
 
     it "shifts alpha characters" do
-      expect(@cypher.decrypt("defg")).to eq "abcd"
+      expect(@cipher.decrypt("defg")).to eq "abcd"
     end
 
     it "wraps after z" do
-      expect(@cypher.decrypt("abc")).to eq "xyz"
+      expect(@cipher.decrypt("abc")).to eq "xyz"
     end
 
     it "handles upper and lower characters" do
-      expect(@cypher.decrypt("dEfAbC")).to eq "aBcXyZ"
+      expect(@cipher.decrypt("dEfAbC")).to eq "aBcXyZ"
     end
 
     it "leaves non alpha characters alone" do
-      expect(@cypher.decrypt("123 ,{|}")).to eq "123 ,{|}"
+      expect(@cipher.decrypt("123 ,{|}")).to eq "123 ,{|}"
     end
   end
 
   it "works with larger shifts" do
-    cypher = CaesarCipher.new(54)
-    expect(cypher.encrypt("abcd")).to eq "cdef"
-    expect(cypher.decrypt("cdef")).to eq "abcd"
+    cipher = CaesarCipher.new(54)
+    expect(cipher.encrypt("abcd")).to eq "cdef"
+    expect(cipher.decrypt("cdef")).to eq "abcd"
   end
 
   it "works with negative shifts" do
-    cypher = CaesarCipher.new(-2)
-    expect(cypher.encrypt("abcd")).to eq "yzab"
-    expect(cypher.decrypt("yzab")).to eq "abcd"
+    cipher = CaesarCipher.new(-2)
+    expect(cipher.encrypt("abcd")).to eq "yzab"
+    expect(cipher.decrypt("yzab")).to eq "abcd"
   end
 
 end
